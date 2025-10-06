302c81bf220ae775a2e675fad5f74e9f31d2764a
AffeDaBoss <alfred2007.erlandsson@gmail.com>
2025-10-06 09:50:20 +0200

    Alfred la till kod

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <string>
 using namespace std;
 
+
+
 int main() {
     string namn;
+    cout << "Hej från <mitt namn>!" << endl;
     cout << "Vad heter du? ";
     getline(cin, namn);
     cout << "Hej " << namn << "!" << endl;
